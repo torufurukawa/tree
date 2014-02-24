41edd06e6a8177424c761926c8d1ac209ea3437c
Toru Furukawa <torufurukawa@gmail.com>
2014-02-25 08:06:41 +0900

    Add tr-focus-me directive

--- js/treeee.js
+++ js/treeee.js
@@ -1,10 +1,23 @@
+// Directives
+
 var app = angular.module("app", ['ui.keypress']);
+
+var app = angular.module("app", ['ui.keypress']).
+  directive('trFocusMe', [function() {
+    return function(scope, elements, attrs) {
+      console.log(scope);
+      console.log(attrs);
+      elements[0].focus();
+    };
+  }]);
+
 
 // Controllers
 
 var TreeCtrl = function($scope) {
     $scope.tree = new Node();
     $scope.newContent = "";
+    $scope.activeNode = -1;
 
     $scope.appendNode = function() {
         $scope.tree.append(new Node($scope.newContent));
@@ -17,8 +30,11 @@ var TreeCtrl = function($scope) {
 
     $scope.stopEditing = function() {
         console.log("stop editing");
+        // blur
+        // activate last item
     };
 };
+
 
 // Node class
 
