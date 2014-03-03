8ab9f6e284b43f489498056de5d1c24308ecca4a
Toru Furukawa <torufurukawa@gmail.com>
2014-03-04 08:08:46 +0900

    Save items on change

--- js/treeee.js
+++ js/treeee.js
@@ -1,20 +1,20 @@
 // Application module
 
-var app = angular.module("app", ['ui.keypress', 'ui.sortable']);
+var app = angular.module('app', ['ui.keypress', 'ui.sortable']);
 
 
 // Controller
 
 var TreeCtrl = function($scope) {
   $scope.items = [];
-  $scope.newContent = "";
+  $scope.newContent = '';
   $scope.activeNode = -1;
 
   $scope.addItem = function() {
     if ($scope.newContent) {
       var item = {content:$scope.newContent, isBeingEdited:false};
       $scope.items.push(item);
-      $scope.newContent = "";
+      $scope.newContent = '';
     }
   };
 
@@ -24,9 +24,26 @@ var TreeCtrl = function($scope) {
 
   $scope.finishEditing = function(item) {
     item.isBeingEdited = false;
+    $scope.save();
   };
 
   $scope.removeItem = function(idx) {
     $scope.items.splice(idx, 1);
   };
+
+  $scope.save = function() {
+    // convert into text
+    var text = '';
+    angular.forEach($scope.items, function(val, i) {
+      text += val.content + '\n';
+    });
+
+    // save on local storage
+    window.localStorage.setItem('tree', text);
+  };
+
+  // save tree on change
+  $scope.$watchCollection('items', function(newValue, oldValue) {
+    $scope.save();
+  });
 };
