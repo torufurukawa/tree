9321150cd318e83f36a89e40a37535ba014f52cb
Toru Furukawa <torufurukawa@gmail.com>
2014-03-13 08:13:27 +0900

    firstChild の処理を追加

--- poc.js
+++ poc.js
@@ -1,18 +1,65 @@
+function indent(n) {
+    var text = "";
+    for (var i=0; i<n; i++) {
+        text += "  ";
+    }
+    return text;
+}
+
+
 function Node(data) {
     this.data = data;
 
-    // TODO: firstChild
+    this.firstChild = null;
+
+    // TODO: lastChild?
     // TODO: nextSibling
     // TODO: previousSibling
     // TODO: parent
 
-    this.toString = function() {
-        var text = '{data:"'+this.data+'"}';
+    this.toString = function(level) {
+        if (!level) {
+            level = 0;
+        }
+
+        var text = "";
+
+        // 自分自身をレンダ
+        text += indent(level) + this.data + "\n";
+
+        // 子ノードをレンダ
+        var node = this.firstChild;
+        while (node) {
+            text += node.toString(level+1)
+            node = node.nextSibling;
+        }
+
         return text;
-    }
+    };
+
+    this.append = function(node) {
+        if (this.firstChild) {
+            // TODO
+        } else {
+            this.firstChild = node;
+        }
+    };
 }
+
+
+// - root
+//   - goal
+//   - wbs
+//   - tasks
+//     - A
+//     - (C)
+//     - D
+//     - B
+//   - update
+
 
 // TODO: new を使わないコンストラクタ
 var root = new Node("root");
+root.append(new Node("goal"));
 
 print(root.toString());
