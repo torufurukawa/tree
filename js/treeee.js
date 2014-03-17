4eb7fa481aa5d5b19398e9ef58a584278403e03a
Toru Furukawa <torufurukawa@gmail.com>
2014-03-18 08:35:54 +0900

    Disable save and load

--- js/treeee.js
+++ js/treeee.js
@@ -23,14 +23,12 @@ var TreeCtrl = function($scope) {
     if (item.level < MAX_LEVEL) {
       item.level += 1;
     }
-    $scope.save();
   }
 
   $scope.dedent = function(item) {
     if (item.level > 0) {
       item.level -= 1;
     }
-    $scope.save();
   }
 
   $scope.startEditing = function(item) {
@@ -39,46 +37,10 @@ var TreeCtrl = function($scope) {
 
   $scope.finishEditing = function(item) {
     item.isBeingEdited = false;
-    $scope.save();
   };
 
   $scope.removeItem = function(idx) {
     $scope.items.splice(idx, 1);
   };
 
-  $scope.save = function() {
-    var tree = [];
-
-    var dump = function(items) {
-      var obj = [];
-      angular.forEach(items, function(item, index) {
-        obj.push({content:item.content, items:dump(item.items)});
-      });
-      return obj;
-    };
-
-    var text = JSON.stringify(dump($scope.items));
-    window.localStorage.setItem('tree', text);
-  };
-
-  // save tree on change
-  $scope.$watchCollection('items', function(newValue, oldValue) {
-    $scope.save();
-  });
-
-  $scope.load = function() {
-    // load from local stroage
-    var text = window.localStorage.getItem('tree');
-
-    // return empty tree if not found
-    if (!text) {
-      return [];
-    }
-
-    // convert text into object
-    return JSON.parse(text);
-  };
-
-  // Initialization
-  $scope.items = $scope.load();
 };
