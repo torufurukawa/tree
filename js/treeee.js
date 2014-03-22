1d71fe2ef8f21f1945ffbbfc0ef7430b9e39ebbd
Toru Furukawa <torufurukawa@gmail.com>
2014-03-22 16:54:14 +0900

    Autofocus when starting edit

--- js/treeee.js
+++ js/treeee.js
@@ -3,11 +3,33 @@
 var app = angular.module('app', ['ui.keypress', 'ui.sortable']);
 
 
+// Directives
+
+app.directive('syncFocusWith', function($timeout, $rootScope) {
+  return {
+    restrict: 'A',
+    scope: {
+      focusValue: "=syncFocusWith"
+    },
+    link: function($scope, $element, attrs) {
+      $scope.$watch("focusValue", function(currentValue, previousValue) {
+        if (currentValue === true && !previousValue) {
+          $element[0].focus();
+        } else if (currentValue === false && previousValue) {
+          $element[0].blur();
+        }
+      })
+    }
+  }
+});
+
+
 // Controllers
 
 var TreeCtrl = function($scope) {
   $scope.items = new TreeElement(null);
   $scope.newContent = '';
+  $scope.latestFocus = {};
 
   $scope.addItem = function() {
     if ($scope.newContent) {
